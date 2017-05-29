b90365b9c73d24c67f16a8fc7dc2bb75b61f3628
Derek William Scott <derek.scott@gmail.com>
2017-05-29 11:59:36 -0700

    Show/hide window buttons when mouse enters/exits the window

--- Viewer/DubsWindow.m
+++ Viewer/DubsWindow.m
@@ -16,6 +16,8 @@
     self.movableByWindowBackground = true;
     self.titleVisibility = NSWindowTitleHidden;
     [self setAppearance:[NSAppearance appearanceNamed:NSAppearanceNameVibrantDark]];
+    
+    [self updateTrackingAreas];
 }
 
 - (void)cancel:(id)sender
@@ -31,6 +33,30 @@
     {
         [[NSNotificationCenter defaultCenter] postNotificationName:@"fileCanceledNotification" object:nil userInfo:nil];
     }
+}
+
+- (void)updateTrackingAreas
+{
+    NSTrackingArea *const trackingArea = [[NSTrackingArea alloc] initWithRect:NSZeroRect options:(NSTrackingMouseEnteredAndExited | NSTrackingActiveAlways | NSTrackingInVisibleRect) owner:self userInfo:nil];
+    [self.contentView addTrackingArea:trackingArea];
+}
+
+- (void)mouseEntered:(NSEvent *)event
+{
+    [super mouseEntered:event];
+    
+    [[self standardWindowButton:NSWindowCloseButton] setHidden:NO];
+    [[self standardWindowButton:NSWindowMiniaturizeButton] setHidden:NO];
+    [[self standardWindowButton:NSWindowZoomButton] setHidden:NO];
+}
+
+- (void)mouseExited:(NSEvent *)event
+{
+    [super mouseExited:event];
+    
+    [[self standardWindowButton:NSWindowCloseButton] setHidden:YES];
+    [[self standardWindowButton:NSWindowMiniaturizeButton] setHidden:YES];
+    [[self standardWindowButton:NSWindowZoomButton] setHidden:YES];
 }
 
 @end
